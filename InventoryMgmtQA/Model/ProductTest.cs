f0cd654ad9d70857fc258630eecb8c64299a243f
nightjourney123 <121560698+nightjourney123@users.noreply.github.com>
2024-09-20 17:12:19 +0800

    Add files via upload

---
+++ InventoryMgmtQA/Model/ProductTest.cs
@@ -0,0 +1,88 @@
+using InventoryMgmt.Model;
+using System.ComponentModel.DataAnnotations;
+
+// guide: https://learn.microsoft.com/en-us/visualstudio/test/walkthrough-creating-and-running-unit-tests-for-managed-code?view=vs-2022
+
+namespace InventoryMgmtQA.Model
+{
+    [TestClass]
+    public class ProductTest
+    {
+        [TestMethod]
+        public void TestAddProduct()
+        {
+            // create a new product with compliant attribute values
+            Product product = new()
+            {
+                Name = "TestProduct",
+                QuantityInStock = 1,
+                Price = 1.0M
+            };
+
+            var results = new List<ValidationResult>();
+            var context = new ValidationContext(product, null, null);
+            bool isProductValid = Validator.TryValidateObject(product, context, results, true);
+
+            // the product must be valid since all attributes values validated correctly
+            Assert.IsTrue(isProductValid);
+        }
+        [TestMethod]
+        public void TestAddProductPriceNegative()
+        {
+            Product product = new()
+            {
+                Name = "TestProduct",
+                QuantityInStock = 1,
+                Price = -1.0M // test for negative price
+            };
+
+            var results = new List<ValidationResult>();
+            var context = new ValidationContext(product, null, null);
+            bool isProductValid = Validator.TryValidateObject(product, context, results, true);
+
+            // the product must NOT be valid since the Price attribute has invalid value
+            Assert.IsFalse(isProductValid);
+        }
+
+        // add more test cases here
+
+        // add a product with a 0 price 
+        [TestMethod]
+        public void TestAddProductZeroPrice()
+        {
+            Product product = new()
+            {
+                Name = "TestProduct",
+                QuantityInStock = 1,
+                Price = 0
+            };
+
+            var results = new List<ValidationResult>();
+            var context = new ValidationContext(product, null, null);
+            bool isProductValid = Validator.TryValidateObject(product, context, results, true);
+
+            Assert.IsTrue(isProductValid);
+        }
+
+        // add a product with negative stock
+        [TestMethod]
+        public void TestAddProductStockNegative()
+        {
+            Product product = new()
+            {
+                Name = "TestProduct",
+                QuantityInStock = -1,
+                Price = 1.0M
+            };
+
+            var results = new List<ValidationResult>();
+            var context = new ValidationContext(product, null, null);
+            bool isProductValid = Validator.TryValidateObject(product, context, results, true);
+
+            Assert.IsFalse(isProductValid);
+        }
+
+
+    
+    }
+}
